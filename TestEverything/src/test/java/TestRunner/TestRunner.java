1343efaf1b80cbafe3235bc3dd6515cdd8b7e3a9
Nirmal Sahoo <nsahoo@mccorp.com>
2021-08-09 11:10:09 +0530

    Test Runner Commit_6_Nirmal

--- TestEverything/src/test/java/TestRunner/TestRunner.java
+++ TestEverything/src/test/java/TestRunner/TestRunner.java
@@ -2,9 +2,8 @@ package TestRunner;
 import org.junit.runner.RunWith;
 import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
-
 @RunWith(Cucumber.class)
-@CucumberOptions(features= {"src/test/resources/Login.feature"},glue= {"DriverFactory","StepDefination"},monochrome=true,plugin= {"pretty","html:Reports/cucumber-reports"})
+@CucumberOptions(features= {"src/test/resources/Login.feature"},glue= {"DriverFactory","StepDefination"},monochrome=true)
 public class TestRunner 
 {
 
